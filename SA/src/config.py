48348a432a3fee9baaddae82917c694a4ca7429e
a.dolgov <dolgov_am@mail.ru>
2025-04-11 16:33:53 +0300

    full config file

--- SA/src/config.py
+++ SA/src/config.py
@@ -1,6 +1,7 @@
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
-class Settings:
+
+class Settings(BaseSettings):
     DB_HOST: str
     DB_PORT: int
     DB_USER: str
@@ -9,5 +10,13 @@ class Settings:
 
     @property
     def DATABASE_URL_asyncpg(self):
-        return f"postgresql+asyncpg://{}{}{}{}{}"
-    ewwrtw
+        return f"postgresql+asyncpg://{self.DB_USER}:{self.DB_PASS}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
+
+    @property
+    def DATABASE_URL_psycopg(self):
+        return f"postgresql+asyncpg://{self.DB_USER}:{self.DB_PASS}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
+
+    model_config = SettingsConfigDict(env_file=".env")
+
+
+setting = Settings()
