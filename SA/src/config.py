17577b22ea7cfd3d79e1aa868e531b3faf5b36f9
a.dolgov <dolgov_am@mail.ru>
2025-04-11 15:46:06 +0300

    add config.py

---
+++ SA/src/config.py
@@ -0,0 +1,12 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+class Settings:
+    DB_HOST: str
+    DB_PORT: int
+    DB_USER: str
+    DB_PASS: str
+    DB_NAME: str
+
+    @property
+    def DATABASE_URL_asyncpg(self):
+        return f"postgresql+asyncpg://{}{}{}{}{}"
